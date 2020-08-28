1fad7ad870c0f324aa7eebff3bf430830173a075
Raxmatilla97 <wi.fi.xor@gmail.com>
2020-08-28 09:21:01 +0500

    Ikkinchi october proyekti

---
+++ vendor/october/rain/tests/fixtures/lang/en/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'test' => [
+        'pagination' => 'Displayed records: :from-:to of :total',
+        'hello_october' => 'Hello October!',
+        'choice' => 'Page|Pages',
+    ],
+];
